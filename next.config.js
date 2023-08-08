0ef75a489bb287f6cccc7de4974b04c06258516e
algomachine007 <amaben202phantom@gmail.com>
2023-08-08 21:19:08 +0100

    config

--- next.config.js
+++ next.config.js
@@ -2,7 +2,14 @@
 const nextConfig = {
   experimental: {
     appDir: true,
-  },
-}
 
-module.exports = nextConfig
+    // for digitalOcean
+    output: "export",
+    distDir: "_static",
+    images: {
+      unoptimized: true,
+    },
+  },
+};
+
+module.exports = nextConfig;
